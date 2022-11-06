4c1b49c4df9cb2302bc86bb98154ffd61130de13
Egor Laptev <laptencio@yandex.ru>
2022-11-07 03:34:54 +0400

    Add constructor, add(), pick(), programm works

--- src/com/kowalski/MagicBox.java
+++ src/com/kowalski/MagicBox.java
@@ -1,8 +1,40 @@
 package com.kowalski;
 
-public class MagicBox <T>{
+import java.util.Random;
 
-    int quantity;
-    
+public class MagicBox <T> {
 
+    private int quantity;
+    private T[] items;
+    Random random = new Random();
+
+    public MagicBox(int quantity) {
+        this.quantity = quantity;
+        this.items = (T[]) new Object[quantity];
+    }
+
+    boolean add(T item) {
+        boolean isAdd = false;
+
+        for (int i = 0; i < items.length; i++) {
+
+            if (items[i] == null) {
+                items[i] = item;
+                isAdd = true;
+                break;
+            }
+        }
+        return isAdd;
+    }
+
+    public T pick(){
+
+        for (int i = 0; i < items.length; i++) {
+            if (items[i] == null){
+                int needAdd = items.length - i;
+                throw new RuntimeException ("Необходимо заполнить коробку! Добавьте ещё " + needAdd + ((needAdd > 4) ? " предметов" : " предмет(а)"));
+            }
+        }
+        return items[random.nextInt(items.length)];
+    }
 }
--- src/com/kowalski/Main.java
+++ src/com/kowalski/Main.java
@@ -3,6 +3,24 @@ package com.kowalski;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    MagicBox <String> box = new MagicBox<>(3);
+
+        box.add("Rock");
+        box.add("Paper");
+        box.add("Scissors");
+        System.out.println(box.add("Scissors2"));
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(box.pick());
+        }
+
+        MagicBox <Integer> rndm = new MagicBox<>(7);
+
+        for (int i = 0; i < 7; i++) {
+            rndm.add(i);
+        }
+        for (int i = 0; i < 5; i++) {
+            System.out.println(rndm.pick());
+        }
     }
 }
